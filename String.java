52e94ae6379b1f5f16de595b95721633d43eafb5
kp3166048-glitch <kp3166048@gmail.com>
2026-01-29 21:04:08 +0530

    Update String.java

--- String.java
+++ String.java
@@ -1,6 +1,6 @@
 public class String{
   public static void main(String[] args){
-    string str="Hello world!";
+    String str="Hello world!";
       int x=str.length();
     System.out.println("Length of String"+x);
   }
