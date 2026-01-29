d0ab741df3ba38e73e16273c9440b4e42316047f
kp3166048-glitch <kp3166048@gmail.com>
2026-01-29 20:58:44 +0530

    Update and rename LenghtOfString.java to String.java

--- LenghtOfString.java
+++
@@ -1,7 +0,0 @@
-public class LenghtOfString {
-  public static void main(String[] args) {
-    String str = "Hello World!";
-      int x=str.length();
-      System.out.println("lenght of string"+x);
-  }
-}
---
+++ String.java
@@ -0,0 +1,7 @@
+public class String{
+  public static void main(String[] args){
+    string str="Hello world!";
+      int x=str.length();
+    System.out.println("Length of String"+x);
+  }
+}
