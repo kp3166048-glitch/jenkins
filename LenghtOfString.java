72ebc4ad9cfeeca9a376ae274dd1a07e556719aa
kp3166048-glitch <kp3166048@gmail.com>
2026-01-29 20:54:16 +0530

    Update LenghtOfString.java

--- LenghtOfString.java
+++ LenghtOfString.java
@@ -1,7 +1,7 @@
 public class LenghtOfString {
   public static void main(String[] args) {
     String str = "Hello World!";
-      int str=str.length();
-      System.out.println("lenght of string"+str);
+      int x=str.length();
+      System.out.println("lenght of string"+x);
   }
 }
