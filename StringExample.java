c8b16e82b07d3e59b1e5fffb5e8abda921629eb3
kp3166048-glitch <kp3166048@gmail.com>
2026-01-29 21:20:04 +0530

    Add MyStringExample.java file

--- StringExample.java
+++
@@ -1,7 +0,0 @@
-public class StringExample{
-  public static void main(String[] args){
-    String str="Hello world!";
-    int x=str.length;
-    System.out.println("Length of String: " + x);
-  }
-}
