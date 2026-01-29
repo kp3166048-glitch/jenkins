ee66f4b6f8927c3eb48ebca2045b22be78c9ac9f
kp3166048-glitch <kp3166048@gmail.com>
2026-01-29 20:51:07 +0530

    Fix output formatting for string length display

--- LenghtOfString.java
+++ LenghtOfString.java
@@ -2,6 +2,6 @@ public class LenghtOfString {
   public static void main(String[] args) {
     String str = "Hello World!";
       int str=str.length();
-      System.out.println("lenght of string",+str);
+      System.out.println("lenght of string"+str);
   }
 }
